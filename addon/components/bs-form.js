f4a20ee77679391139f24cfbfdcb0a40ea7a9fdb
Etienne F <etienne.florit@tagcommander.com>
2017-03-02 13:57:25 +0100

    Refactoring

--- addon/components/bs-form.js
+++ addon/components/bs-form.js
@@ -1,22 +1,12 @@
 import Ember from 'ember';
 import BsForm from 'ember-bootstrap/components/bs-form';
 
-const {
-  computed
-} = Ember;
+const { computed } = Ember;
 
 export default BsForm.extend({
   hasValidator: computed.notEmpty('model.validate'),
 
-  validate(model) {
-    return new Ember.RSVP.Promise((resolve, reject) => {
-      let m = model;
-
-      if(model instanceof Ember.ObjectProxy) {
-        m = model.get('content');
-      }
-
-      m.validate().then(() => model.get('validations.isTruelyValid') ? resolve() : reject(), reject);
-    });
+  validate(/*model*/) {
+    return new Ember.RSVP.Promise();
   }
 });
--- addon/components/bs-form/element.js
+++ addon/components/bs-form/element.js
@@ -1,26 +1,30 @@
 import Ember from 'ember';
 import BsFormElement from 'ember-bootstrap/components/bs-form/element';
 
-const { computed, defineProperty } = Ember;
+const { computed, defineProperty, isPresent } = Ember;
 
 export default BsFormElement.extend({
   setupValidations() {
-  	let errorProperty = `model.errors.${this.get('property')}`;
-    defineProperty(this, 'errors', computed(errorProperty, () => {
-    	let errors = this.get(errorProperty);
+    let propertyErrors = `model.errors.${this.get('property')}`;
 
-    	this.set('showValidation', !!errors);
-    	if (errors) {
-	    	return errors.map(function(error) {
-	    		return error.message;
-	    	});
-    	} else {
-    		return null;
-    	}
+    defineProperty(this, 'errors', computed(propertyErrors, () => {
+      let errors = this.get(propertyErrors);
+      let hasErrors = isPresent(errors);
+      let errorMessages = null;
+
+      this.set('showValidation', hasErrors);
+
+      if (hasErrors) {
+        errorMessages = errors.map(function(error) {
+          return error.message;
+        });
+      }
+
+      return errorMessages;
     }));
 
     defineProperty(this, 'validation', computed('errors', () => {
-    	return this.get('errors') ? 'error' : null;
+      return isPresent(this.get('errors')) ? 'error' : null;
     }));
   }
 });
